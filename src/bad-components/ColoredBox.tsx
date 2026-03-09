210a7bd631f32ea685e586d8033d50dbfe8de0b5
Zachary <zacharyz@udel.edu>
2026-03-08 21:41:08 -0400

    Simplify component structure to remove nested function components

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -7,41 +7,29 @@ const DEFAULT_COLOR_INDEX = 0;
 export function ColoredBox(): React.JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
 
-    function ChangeColor(): React.JSX.Element {
-        return (
-            <Button
-                onClick={() => {
-                    setColorIndex((prev) => (1 + prev) % COLORS.length);
-                }}
-            >
-                Next Color
-            </Button>
-        );
-    }
-
-    function ColorPreview(): React.JSX.Element {
-        return (
-            <div
-                data-testid="colored-box"
-                style={{
-                    width: "50px",
-                    height: "50px",
-                    backgroundColor: COLORS[colorIndex],
-                    display: "inline-block",
-                    verticalAlign: "bottom",
-                    marginLeft: "5px",
-                }}
-            ></div>
-        );
-    }
-
     return (
         <div>
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <Button
+                    onClick={() => {
+                        setColorIndex((prev) => (1 + prev) % COLORS.length);
+                    }}
+                >
+                    Next Color
+                </Button>
+                <div
+                    data-testid="colored-box"
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        backgroundColor: COLORS[colorIndex],
+                        display: "inline-block",
+                        verticalAlign: "bottom",
+                        marginLeft: "5px",
+                    }}
+                ></div>
             </div>
         </div>
     );
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -4,8 +4,12 @@ import { Button } from "react-bootstrap";
 export function DoubleHalf(): React.JSX.Element {
     const [value, setValue] = useState<number>(10);
 
-    function Doubler(): React.JSX.Element {
-        return (
+    return (
+        <div>
+            <h3>Double Half</h3>
+            <div>
+                The current value is: <span>{value}</span>
+            </div>
             <Button
                 onClick={() => {
                     setValue((prev) => 2 * prev);
@@ -13,11 +17,6 @@ export function DoubleHalf(): React.JSX.Element {
             >
                 Double
             </Button>
-        );
-    }
-
-    function Halver(): React.JSX.Element {
-        return (
             <Button
                 onClick={() => {
                     setValue((prev) => 0.5 * prev);
@@ -25,17 +24,6 @@ export function DoubleHalf(): React.JSX.Element {
             >
                 Halve
             </Button>
-        );
-    }
-
-    return (
-        <div>
-            <h3>Double Half</h3>
-            <div>
-                The current value is: <span>{value}</span>
-            </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
         </div>
     );
 }
