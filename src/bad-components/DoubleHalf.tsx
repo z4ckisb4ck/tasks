496dc08bcc01d04a2f57561893bb2ec1ca044065
Zachary <zacharyz@udel.edu>
2026-03-08 21:32:41 -0400

    Fix all four broken components: DoubleHalf, ChooseTeam, ColoredBox, ShoveBox

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,37 +1,38 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
-
-function Doubler(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(2 * dhValue);
-            }}
-        >
-            Double
-        </Button>
-    );
-}
-
-function Halver(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(0.5 * dhValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
-}
 
 export function DoubleHalf(): React.JSX.Element {
+    const [value, setValue] = useState<number>(10);
+
+    function Doubler(): React.JSX.Element {
+        return (
+            <Button
+                onClick={() => {
+                    setValue(2 * value);
+                }}
+            >
+                Double
+            </Button>
+        );
+    }
+
+    function Halver(): React.JSX.Element {
+        return (
+            <Button
+                onClick={() => {
+                    setValue(0.5 * value);
+                }}
+            >
+                Halve
+            </Button>
+        );
+    }
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
-                The current value is: <span>{dhValue}</span>
+                The current value is: <span>{value}</span>
             </div>
             <Doubler></Doubler>
             <Halver></Halver>
