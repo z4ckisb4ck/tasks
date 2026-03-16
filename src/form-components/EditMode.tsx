8122dd57a96752318ac47d3c32224fde28ea60d4
Zachary <zacharyz@udel.edu>
2026-03-15 23:03:10 -0400

    solved

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,45 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                label="Edit Mode"
+                checked={isEditMode}
+                onChange={(event) => {
+                    setIsEditMode(event.target.checked);
+                }}
+            />
+            {isEditMode ?
+                <div>
+                    <Form.Control
+                        type="text"
+                        value={name === "Your Name" ? "" : name}
+                        onChange={(event) => {
+                            setName(event.target.value);
+                        }}
+                    />
+                    <Form.Check
+                        id="student-checkbox"
+                        type="checkbox"
+                        label="Student"
+                        checked={isStudent}
+                        onChange={(event) => {
+                            setIsStudent(event.target.checked);
+                        }}
+                    />
+                </div>
+            :   <div>
+                    {name} is {isStudent ? "a student" : "not a student"}
+                </div>
+            }
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selectedChoice, setSelectedChoice] = useState<string>(options[0]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Select
+                value={selectedChoice}
+                onChange={(event) => {
+                    setSelectedChoice(event.target.value);
+                }}
+            >
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </Form.Select>
+            <div>{selectedChoice === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
